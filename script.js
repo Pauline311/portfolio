cf909ad63ec4a3c409f382955b3b180f24cfaca8
Pauline Ponon <paulineponon05@gmail.com>
2025-04-18 11:46:45 +0800

    dark mode embeded

--- script.js
+++ script.js
@@ -93,3 +93,21 @@ window.addEventListener('scroll', () => {
         ticking = true;
     }
 });
+const faders = document.querySelectorAll('.fade-in-section');
+
+const appearOptions = {
+  threshold: 0.1,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add("visible");
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
